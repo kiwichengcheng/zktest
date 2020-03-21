59cb77547a89554578c6c946c1a54fa0bdb4cda7
kiwicheng <kiwichengcheng@163.com>
2020-03-21 21:54:01 +0800

    first commit

---
+++ src/main/java/com/example/zktest/ZktestApplication.java
@@ -0,0 +1,13 @@
+package com.example.zktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZktestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zktest/ZktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZktestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
